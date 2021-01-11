83a3a39f21c718275c8e8e9c0195bd939e10c342
Katie Sloan <katie.a.sloan@gmail.com>
2021-01-11 17:19:48 +0000

    can get developer name

---
+++ src/test/java/DeveloperTest.java
@@ -0,0 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+import techStaff.Developer;
+
+import static org.junit.Assert.assertEquals;
+
+public class DeveloperTest {
+
+    Developer developer;
+
+    @Before
+    public void before(){
+        developer = new Developer("Gareth Keenan", "XV123401A", 30000.00);
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Gareth Keenan", developer.getName());
+    }
+
+}
