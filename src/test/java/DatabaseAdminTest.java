cbfca1687acadaaeadeb7074e16f26ad0f20d90c
Katie Sloan <katie.a.sloan@gmail.com>
2021-01-11 20:35:49 +0000

    can get database admin name

--- src/test/java/DatabaseAdminTest.java
+++ src/test/java/DatabaseAdminTest.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+import techStaff.DatabaseAdmin;
+
+import static org.junit.Assert.assertEquals;
+
 public class DatabaseAdminTest {
+
+    DatabaseAdmin databaseAdmin;
+
+    @Before
+    public void before(){
+        databaseAdmin = new DatabaseAdmin ("Tim Canterbury", "ZQ432101A", 25000.00);
+    }
+
+    @Test
+    public void canGetName() {
+        assertEquals("Tim Canterbury", databaseAdmin.getName());
+    }
+
 }
